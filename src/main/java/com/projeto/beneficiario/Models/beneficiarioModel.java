cff8dee8e1bff295d6bd4031c422fe0533561d67
EduardGuilherme <eduguilherme48@gmail.com>
2023-11-06 20:05:15 -0300

    add models

---
+++ src/main/java/com/projeto/beneficiario/Models/beneficiarioModel.java
@@ -0,0 +1,82 @@
+package com.projeto.beneficiario.Models;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "beneficiario")
+public class beneficiarioModel implements Serializable {
+    private static final long SerialVersion = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID beneficiarioId;
+    private String nome;
+    private String telefone;
+    private Date dataNascimento;
+    private  Date dataInclusao;
+    private Date dataAtualizacao;
+
+    public beneficiarioModel() {
+    }
+
+    public beneficiarioModel(UUID beneficiarioId, String nome, String telefone, Date dataNascimento, Date dataInclusao, Date dataAtualizacao) {
+        this.beneficiarioId = beneficiarioId;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.dataInclusao = dataInclusao;
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public UUID getBeneficiarioId() {
+        return beneficiarioId;
+    }
+
+    public void setBeneficiarioId(UUID beneficiarioId) {
+        this.beneficiarioId = beneficiarioId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataInclusao() {
+        return dataInclusao;
+    }
+
+    public void setDataInclusao(Date dataInclusao) {
+        this.dataInclusao = dataInclusao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
---
+++ src/main/java/com/projeto/beneficiario/Models/documentoModel.java
@@ -0,0 +1,72 @@
+package com.projeto.beneficiario.Models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+public class documentoModel implements Serializable {
+    private static final long SerialVersionID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID documentoId;
+    private String tipoDocumento;
+    private String descricao;
+    private Date dataInclusao;
+    private Date dataAtualizacao;
+
+    public documentoModel() {
+    }
+
+    public documentoModel(UUID documentoId, String tipoDocumento, String descricao, Date dataInclusao, Date dataAtualizacao) {
+        this.documentoId = documentoId;
+        this.tipoDocumento = tipoDocumento;
+        this.descricao = descricao;
+        this.dataInclusao = dataInclusao;
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public UUID getDocumentoId() {
+        return documentoId;
+    }
+
+    public void setDocumentoId(UUID documentoId) {
+        this.documentoId = documentoId;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataInclusao() {
+        return dataInclusao;
+    }
+
+    public void setDataInclusao(Date dataInclusao) {
+        this.dataInclusao = dataInclusao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
