34f87e0422d4652f0741de3648c09862baf8eb33
EduardGuilherme <eduguilherme48@gmail.com>
2023-11-07 16:31:09 -0300

    update beneficiarioDTO

--- src/main/java/com/projeto/beneficiario/DTO/BeneficiarioDTO.java
+++ src/main/java/com/projeto/beneficiario/DTO/BeneficiarioDTO.java
@@ -1,16 +1,19 @@
 package com.projeto.beneficiario.DTO;
 
 import com.projeto.beneficiario.Models.BeneficiarioModel;
+import com.projeto.beneficiario.Models.DocumentoModel;
 
 import java.util.Date;
+import java.util.List;
 
 public record BeneficiarioDTO(String nome,
                               String telefone,
                               Date dataNascimento,
                               Date dataInclusao,
-                              Date dataAtualizacao) {
+                              Date dataAtualizacao,
+                              List<DocumentoModel> documentos) {
 
     public BeneficiarioDTO(BeneficiarioModel beneficiarioModel){
-        this(beneficiarioModel.getNome(),beneficiarioModel.getTelefone(),beneficiarioModel.getDataNascimento(),beneficiarioModel.getDataInclusao(),beneficiarioModel.getDataAtualizacao());
+        this(beneficiarioModel.getNome(),beneficiarioModel.getTelefone(),beneficiarioModel.getDataNascimento(),beneficiarioModel.getDataInclusao(),beneficiarioModel.getDataAtualizacao(),beneficiarioModel.getDocumentos());
     }
 }
