f61b7e308424d5df144b5a7173fd1333b4c2b4b2
EduardGuilherme <eduguilherme48@gmail.com>
2023-11-07 14:32:25 -0300

    update documentorepository

--- src/main/java/com/projeto/beneficiario/Repository/DocumentoRepository.java
+++ src/main/java/com/projeto/beneficiario/Repository/DocumentoRepository.java
@@ -3,7 +3,9 @@ package com.projeto.beneficiario.Repository;
 import com.projeto.beneficiario.Models.DocumentoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DocumentoRepository extends JpaRepository<DocumentoModel, UUID> {
+    List<DocumentoModel> findByBeneficiarioModel_BeneficiarioId(UUID id);
 }
