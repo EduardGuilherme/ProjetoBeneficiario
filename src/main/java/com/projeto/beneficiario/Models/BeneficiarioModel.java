d319c4ddd88c1531c72bde84e9ceb2463b6c162d
EduardGuilherme <eduguilherme48@gmail.com>
2023-11-07 16:45:15 -0300

    update beneficiario model

--- src/main/java/com/projeto/beneficiario/Models/BeneficiarioModel.java
+++ src/main/java/com/projeto/beneficiario/Models/BeneficiarioModel.java
@@ -13,7 +13,7 @@ public class BeneficiarioModel implements Serializable {
     private static final long SerialVersion = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID beneficiarioId;
     private String nome;
     private String telefone;
