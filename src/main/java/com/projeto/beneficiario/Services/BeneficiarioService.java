7d3075dcbf3e80fd32ab4b2647facf43457ef1e2
EduardGuilherme <eduguilherme48@gmail.com>
2023-11-08 19:39:01 -0300

    update beneficiarioservice

--- src/main/java/com/projeto/beneficiario/Services/BeneficiarioService.java
+++ src/main/java/com/projeto/beneficiario/Services/BeneficiarioService.java
@@ -1,6 +1,7 @@
 package com.projeto.beneficiario.Services;
 
 import com.projeto.beneficiario.DTO.BeneficiarioDTO;
+import com.projeto.beneficiario.DTO.DocumentoDTO;
 import com.projeto.beneficiario.Models.BeneficiarioModel;
 import com.projeto.beneficiario.Models.DocumentoModel;
 import com.projeto.beneficiario.Repository.BeneficiarioRepository;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -25,23 +27,33 @@ public class BeneficiarioService {
     }
 
     @Transactional
-    public BeneficiarioModel saveBeneficiarioDocumento(BeneficiarioModel beneficiarioModel, List<DocumentoModel> documentos){
+    public BeneficiarioModel saveBeneficiarioDocumento(BeneficiarioDTO beneficiarioDTO){
+        BeneficiarioModel beneficiarioModel = new BeneficiarioModel();
+        beneficiarioModel.setNome(beneficiarioDTO.getNome());
+        beneficiarioModel.setTelefone(beneficiarioDTO.getTelefone());
+        beneficiarioModel.setDataNascimento(beneficiarioDTO.getDataNascimento());
+
+        List<DocumentoModel> documentos = new ArrayList<>();
+        for (DocumentoDTO documentoDTO : beneficiarioDTO.getDocumentos()) {
+            DocumentoModel documentoModel = new DocumentoModel();
+            documentoModel.setTipoDocumento(documentoDTO.getTipoDocumento());
+            documentoModel.setDescricao(documentoDTO.getDescricao());
+            documentoModel.setbeneficiariomodel(beneficiarioModel);
+            documentos.add(documentoModel);
+        }
+
         beneficiarioModel.setDocumentos(documentos);
-        BeneficiarioModel saveBeneficiario = beneficiarioRepository.save(beneficiarioModel);
 
-        documentos.forEach(documento -> documento.setbeneficiariomodel(saveBeneficiario));
-        documentoRepository.saveAll(documentos);
-
-        return saveBeneficiario;
+        return beneficiarioRepository.save(beneficiarioModel);
     }
 
     public BeneficiarioModel updateBeneficiario(UUID id, BeneficiarioDTO beneficiarioDTO){
         Optional<BeneficiarioModel> beneficiarioExist = beneficiarioRepository.findById(id);
         if(beneficiarioExist.isPresent()){
             BeneficiarioModel existbeneficiario = beneficiarioExist.get();
-            existbeneficiario.setNome(beneficiarioDTO.nome());
-            existbeneficiario.setDataNascimento(beneficiarioDTO.dataNascimento());
-            existbeneficiario.setTelefone(beneficiarioDTO.telefone());
+            existbeneficiario.setNome(beneficiarioDTO.getNome());
+            existbeneficiario.setDataNascimento(beneficiarioDTO.getDataNascimento());
+            existbeneficiario.setTelefone(beneficiarioDTO.getTelefone());
 
             return beneficiarioRepository.save(existbeneficiario);
         }else{
