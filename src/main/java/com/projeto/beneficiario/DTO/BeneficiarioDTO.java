70985b2e0ff4e419c3b2053488e18cee0f8b722a
EduardGuilherme <eduguilherme48@gmail.com>
2023-11-08 19:38:16 -0300

    update DTO

--- src/main/java/com/projeto/beneficiario/DTO/BeneficiarioDTO.java
+++ src/main/java/com/projeto/beneficiario/DTO/BeneficiarioDTO.java
@@ -6,15 +6,43 @@ import com.projeto.beneficiario.Models.DocumentoModel;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public record BeneficiarioDTO(String nome,
-                              String telefone,
-                              LocalDate dataNascimento,
-                              LocalDate dataInclusao,
-                              LocalDate dataAtualizacao,
-                              List<DocumentoModel> documentos) {
+public class BeneficiarioDTO{
+    private String nome;
+    private String telefone;
+    private LocalDate dataNascimento;
+    private List<DocumentoDTO> documentos;
 
-    public BeneficiarioDTO(BeneficiarioModel beneficiarioModel){
-        this(beneficiarioModel.getNome(),beneficiarioModel.getTelefone(),beneficiarioModel.getDataNascimento(),beneficiarioModel.getDataInclusao(),beneficiarioModel.getDataAtualizacao(),beneficiarioModel.getDocumentos());
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public List<DocumentoDTO> getDocumentos() {
+        return documentos;
+    }
+
+    public void setDocumentos(List<DocumentoDTO> documentos) {
+        this.documentos = documentos;
     }
 }
--- src/main/java/com/projeto/beneficiario/DTO/DocumentoDTO.java
+++ src/main/java/com/projeto/beneficiario/DTO/DocumentoDTO.java
@@ -1,18 +1,23 @@
 package com.projeto.beneficiario.DTO;
 
-import com.projeto.beneficiario.Models.BeneficiarioModel;
-import com.projeto.beneficiario.Models.DocumentoModel;
 
-import java.time.LocalDate;
-import java.util.Date;
+public class DocumentoDTO {
+    private String tipoDocumento;
+    private String descricao;
 
-public record DocumentoDTO(String tipoDocumento,
-                           String descricao,
-                           LocalDate dataInclusao,
-                           LocalDate dataAtualizacao,
-                           BeneficiarioModel beneficiarioModel) {
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
 
-    public DocumentoDTO(DocumentoModel documentoModel){
-        this(documentoModel.getTipoDocumento(),documentoModel.getDescricao(),documentoModel.getDataInclusao(),documentoModel.getDataAtualizacao(),documentoModel.getbeneficiariomodel());
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
